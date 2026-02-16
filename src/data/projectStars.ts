07564e755e1b01ad091b61a973c4315b36401f7f
orangekame3 <49517624+orangekame3@users.noreply.github.com>
2026-02-16 03:50:19 +0000

    chore: update GitHub stars

--- src/data/projectStars.ts
+++ src/data/projectStars.ts
@@ -1,10 +1,10 @@
 export const githubStars: Record<string, number | null> = {
   'future-architect/tftarget': 274,
   'oqtopus-team/device-gateway': 9,
-  'oqtopus-team/oqtopus-cloud': 108,
+  'oqtopus-team/oqtopus-cloud': 109,
   'oqtopus-team/qdash': 22,
-  'orangekame3/ghfetch': 61,
-  'orangekame3/mirrormate': 453,
+  'orangekame3/ghfetch': 62,
+  'orangekame3/mirrormate': 457,
   'orangekame3/paclear': 228,
   'orangekame3/qasmfmt': 0,
   'orangekame3/qasmtools': 7,
